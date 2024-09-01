bc8791cfb8e7d458da71f703efa3e003e8d03054
rashiniashinsana <rashinivithanage2@gmail.com>
2024-09-01 10:04:30 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/gdse68/inclassassignment/InClassAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.inclassassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InClassAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
